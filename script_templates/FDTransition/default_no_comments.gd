28b44127782b122385c8931d0fea76a9dba3d2f3
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-06-19 01:19:52 -0300

    feat: Create template script with no comments for FDTransition

---
+++ script_templates/FDTransition/default_no_comments.gd
@@ -0,0 +1,22 @@
+@tool
+extends FDTransition
+
+
+func _ready() -> void:
+	pass
+
+
+func _setup_play_in() -> void:
+	pass
+
+
+func _setup_play_out() -> void:
+	pass
+
+
+func _on_play_in(ratio: float) -> void:
+	pass
+
+
+func _on_play_out(ratio: float) -> void:
+	pass
